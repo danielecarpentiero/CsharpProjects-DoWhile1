847f93e2d81da53dcec649b8b6a192b8e37ca483
Daniele Carpentiero <carpentierodaniele@gmail.com>
2023-11-02 12:20:12 +0100

    first commit

---
+++ TestProjects/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Inserire un numero intero compreso fra 5 e 10.");
+int value;
+
+do
+{
+    string? input = Console.ReadLine();
+
+    if (!int.TryParse(input, out value))
+    {
+        Console.WriteLine("Mi dispiace, hai inserito un valore errato, riprova.");
+    }
+    else if (value < 5 || value > 10)
+    {
+        Console.WriteLine($"Hai inserito {value}. Per favore digita un valore compreso fra 5 e 10.");
+    }
+} while (value < 5 || value > 10);
+
+if (value >= 5 || value <= 10)
+{
+
+    Console.WriteLine($"Il valore che hai inserito, {value} è stato accettato.");
+}
